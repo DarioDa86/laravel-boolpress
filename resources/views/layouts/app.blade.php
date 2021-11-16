f5f7479d9c40394fe51d9b46a6d2ef88350cc0aa
DarioDa86 <dario.dagnelli@gmail.com>
2021-11-16 19:03:08 +0100

    terminata parte "create" del singolo post

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li><a href="">Tutti i post</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
