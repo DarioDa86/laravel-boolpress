0817f90452131887102b82b76c2f50a4bdcca72d
DarioDa86 <dario.dagnelli@gmail.com>
2021-11-16 19:53:28 +0100

    Corretta parte create

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Post;
 
 class PostController extends Controller
@@ -27,7 +28,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view("admin.posts.create");
     }
 
     /**
@@ -38,7 +39,33 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+
+            'title' => 'string|required|max:100',
+            'content'=> 'string|required'
+        ]);
+
+        $newPost = new Post();
+        $newPost->fill($request->all());
+
+        $slug = Str::of($request -> title)->slug('-');
+
+        $postExist = Post::where("slug", $slug)->first();
+        
+        $count = 2;
+
+        while($postExist) {
+            $slug = Str::of($request -> title)->slug('-') . "-{$count}";
+            $postExist = Post::where("slug", $slug)->first();
+            $count++;
+        }
+        
+        $newPost->slug = $slug;
+
+        $newPost->save();
+
+        return redirect()->route("admin.posts.index")->with('success', "Il post è statp creato");
+
     }
 
     /**
