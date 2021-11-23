6d3b492e857bc9eedefab1bd2dac8e588770b96e
DarioDa86 <dario.dagnelli@gmail.com>
2021-11-23 09:17:20 +0100

    Inserita select anche in edit

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -27,12 +27,15 @@
                         </div>
                         <div class="form-group">
                             <label for="category">Categoria</label>
-                            <select name="category_id" class="form-control">
+                            <select name="category_id" class="form-control @error('category_id') is-invalid @enderror">
                                 <option value="">--- Seleziona una categoria ---</option>
                                 @foreach ($categories as $category)
-                                    <option value="{{$category["id"]}}">{{$category["name"]}}</option>
+                                    <option {{old("catgory_id") == $category["id"] ? 'selected' : null}} value="{{$category["id"]}}">{{$category["name"]}}</option>
                                 @endforeach
                             </select>
+                            @error('category_id')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <button type="submit" class="btn btn-primary">Crea</button>
                     </form>
